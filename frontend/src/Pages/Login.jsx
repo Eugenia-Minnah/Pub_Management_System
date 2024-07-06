96b1812f74f221c9058726c5d3781dfc68b06d6e
Eugenia Minnah <eaminnah@st.ug.edu.gh>
2024-07-06 07:39:06 +0000

    Added code to the Login Screen

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import { FaUserAlt } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
 
 export const Login = () => {
 return (
-    <div>Login</div>
+    <div className='wrapper'>
+        <form action=''>
+            <h1>Login</h1>
+            <div className='input-box'>
+                <input type='text' placeholder='Username' required/>
+                <FaUserAlt />
+            </div>
+            <div className='input-box'>
+                <input type='password' placeholder='Password' required />
+                <FaLock />
+            </div>
+            <div className='remember-forgot'>
+                <label htmlFor=''><input type='checkbox'/>Remember me</label>
+                <a href='#'>Forgot password</a>
+            </div>
+            <button type='submit'>Login</button>
+            <div className='register-link'>
+                <p>Don't have an account?<a href='#'>Register</a></p>
+            </div>
+        </form>
+    </div>
 )
 }
