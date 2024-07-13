34b79e89a79f22586d25fb979a7eab11d62ce48f
naa-adjorkor <naakrong_adjei@st.ug.edu.gh>
2024-07-13 02:43:00 +0000

    updated login and signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import { Login } from './Pages/Login';
+import { Signup } from './Pages/Signup';
 import { Admin } from './Admin/Admin'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Admin/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Signup" element={<Signup />} />
+        <Route path="/Admin" element={<Admin/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
